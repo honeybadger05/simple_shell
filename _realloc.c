bbc4c175351bd801984434b7566c4294025c97ae
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2024-08-29 13:54:59 +0100

    fix

--- _realloc.c
+++ _realloc.c
@@ -1,31 +1,50 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated
- * @size: new size in bytes of the allocated space
+ * _realloc - Reallocates a memory block.
+ * @ptr: Pointer to the old memory block.
+ * @new_size: New size of the memory block.
  *
- * Return: pointer to the new memory block
+ * Return: Pointer to the new memory block, otherwise NULL on failure.
  */
-void *_realloc(void *ptr, size_t size)
-{
-	void *new_ptr = NULL;
 
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(size);
-		if (new_ptr == NULL)
-			return (NULL);
-		return (new_ptr);
-	}
-	if (size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	new_ptr = malloc(size);
-	if (new_ptr == NULL)
-		return (NULL);
+void *_realloc(void *ptr, size_t new_size)
+{
+size_t prev_size, element_size, total_elements, i;
+void *new_ptr;
+char *prev_content, *new_content;
+if (new_size == 0)  /* Checks if new_size is NULL and allocate new memory */
+{
 	free(ptr);
+	return (NULL);
+}
+if (ptr == NULL)  /* Checks if ptr is NULL and if so, allocate new memory */
+	return (malloc(new_size));
+new_ptr = malloc(new_size); /* Attempt to allocate memory for the new block */
+
+if (new_ptr)
+{
+	/* Calculate the size of each element and the total number of elements */
+	element_size = sizeof(((char *)ptr)[0]);
+	total_elements = new_size / element_size;
+	prev_size = total_elements * element_size;
+	/* Use the smaller of the old and new sizes */
+	if (new_size < prev_size)
+	{
+		prev_size = new_size;
+	}
+	prev_content = (char *)ptr;
+	new_content = (char *)new_ptr;
+	/* Copy data from the old block to the new block */
+	for (i = 0; i < prev_size; i++)
+	{
+		new_content[i] = prev_content[i];
+	}
+	free(ptr); /* Free the old block */
 	return (new_ptr);
+}
+else
+{
+	return (NULL); /* Failed allocation */
+}
 }
